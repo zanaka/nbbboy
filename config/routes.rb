3c13dcb2d961c492fa251b3cfae058edb5635af4
zanaka <e155752@ie.u-ryukyu.ac.jp>
2017-10-30 23:59:13 +0900

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'top' => "home#top"
+
+  get 'about' => "home#about"
+
   get 'sample/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
